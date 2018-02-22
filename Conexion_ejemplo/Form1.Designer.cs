c3735be63a16364e7e2274df8ccf928d237dfc79
yaedev <yaehime3@gmail.com>
2018-02-22 09:47:12 -0600

    Insert ready

--- Conexion_ejemplo/Form1.Designer.cs
+++ Conexion_ejemplo/Form1.Designer.cs
@@ -28,53 +28,114 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.dataGridViewData = new System.Windows.Forms.DataGridView();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.textBoxLocation = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBoxNames = new System.Windows.Forms.ComboBox();
+            this.buttonModify = new System.Windows.Forms.Button();
+            this.buttonRemove = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewData)).BeginInit();
             this.SuspendLayout();
             // 
-            // dataGridView1
+            // dataGridViewData
             // 
-            this.dataGridView1.AllowUserToAddRows = false;
-            this.dataGridView1.AllowUserToDeleteRows = false;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 38);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.ReadOnly = true;
-            this.dataGridView1.Size = new System.Drawing.Size(354, 181);
-            this.dataGridView1.TabIndex = 0;
+            this.dataGridViewData.AllowUserToAddRows = false;
+            this.dataGridViewData.AllowUserToDeleteRows = false;
+            this.dataGridViewData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewData.Location = new System.Drawing.Point(12, 100);
+            this.dataGridViewData.Name = "dataGridViewData";
+            this.dataGridViewData.ReadOnly = true;
+            this.dataGridViewData.RowHeadersVisible = false;
+            this.dataGridViewData.Size = new System.Drawing.Size(237, 181);
+            this.dataGridViewData.TabIndex = 0;
+            this.dataGridViewData.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewData_CellClick);
             // 
-            // button1
+            // buttonAdd
             // 
-            this.button1.Location = new System.Drawing.Point(291, 9);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Consulta";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.buttonAdd.Location = new System.Drawing.Point(12, 71);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(75, 23);
+            this.buttonAdd.TabIndex = 1;
+            this.buttonAdd.Text = "Agregar";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.button1_Click);
             // 
-            // textBox1
+            // textBoxLocation
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 12);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(273, 20);
-            this.textBox1.TabIndex = 2;
-            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            this.textBoxLocation.Location = new System.Drawing.Point(73, 45);
+            this.textBoxLocation.Name = "textBoxLocation";
+            this.textBoxLocation.Size = new System.Drawing.Size(176, 20);
+            this.textBoxLocation.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Ubicacion";
+            // 
+            // comboBoxNames
+            // 
+            this.comboBoxNames.FormattingEnabled = true;
+            this.comboBoxNames.Items.AddRange(new object[] {
+            "Altiplano",
+            "Centro",
+            "Poniente"});
+            this.comboBoxNames.Location = new System.Drawing.Point(73, 12);
+            this.comboBoxNames.Name = "comboBoxNames";
+            this.comboBoxNames.Size = new System.Drawing.Size(176, 21);
+            this.comboBoxNames.TabIndex = 5;
+            // 
+            // buttonModify
+            // 
+            this.buttonModify.Location = new System.Drawing.Point(93, 71);
+            this.buttonModify.Name = "buttonModify";
+            this.buttonModify.Size = new System.Drawing.Size(75, 23);
+            this.buttonModify.TabIndex = 6;
+            this.buttonModify.Text = "Modificar";
+            this.buttonModify.UseVisualStyleBackColor = true;
+            this.buttonModify.Click += new System.EventHandler(this.buttonModify_Click);
+            // 
+            // buttonRemove
+            // 
+            this.buttonRemove.Location = new System.Drawing.Point(174, 71);
+            this.buttonRemove.Name = "buttonRemove";
+            this.buttonRemove.Size = new System.Drawing.Size(75, 23);
+            this.buttonRemove.TabIndex = 7;
+            this.buttonRemove.Text = "Eliminar";
+            this.buttonRemove.UseVisualStyleBackColor = true;
+            this.buttonRemove.Click += new System.EventHandler(this.buttonRemove_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.ClientSize = new System.Drawing.Size(378, 231);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.dataGridView1);
+            this.ClientSize = new System.Drawing.Size(263, 293);
+            this.Controls.Add(this.buttonRemove);
+            this.Controls.Add(this.buttonModify);
+            this.Controls.Add(this.comboBoxNames);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxLocation);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.dataGridViewData);
             this.Name = "Form1";
             this.Text = "Ejemplo SQL Server";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewData)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -82,9 +143,14 @@
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridViewData;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.TextBox textBoxLocation;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBoxNames;
+        private System.Windows.Forms.Button buttonModify;
+        private System.Windows.Forms.Button buttonRemove;
     }
 }
 
--- Conexion_ejemplo/Form1.cs
+++ Conexion_ejemplo/Form1.cs
@@ -14,21 +14,64 @@ namespace Conexion_ejemplo
     public partial class Form1 : Form
     {
         private OleDbConnection connection;
+        private string connectionstring, server, database;
+        private OleDbCommand command;//Comando sql
+        private OleDbDataAdapter adapter;//Adapta la estructura al manejador
+        private DataTable table;
 
         public Form1()
         {
             InitializeComponent();
+            server = @"DESKTOP-Q77ELOA\SQLEXPRESS";
+            connectionstring = @"Provider=SQLNCLI11;Server=DESKTOP-Q77ELOA\SQLEXPRESS;Database=universidad;Trusted_Connection=yes";
+            connection = new OleDbConnection(connectionstring);
+            this.UpdateGrid();
+        }
+
+        private void buttonModify_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonRemove_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridViewData_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            connection = new OleDbConnection(@"Provider=SQLNCLI11;Server=DESKTOP-Q77ELOA\SQLEXPRESS;Database=universidad; Trusted_Connection = yes;");
+            string name = comboBoxNames.SelectedItem.ToString();
+            string location = textBoxLocation.Text;
+            connection.Open();
+
+            if (connection.State != ConnectionState.Open)
+                return;
+
+            command = new OleDbCommand("INSERT INTO Informacion_universidad.Zona (nombre, ubicacion) VALUES ('" + name + "','" + location + "')", connection);
+            command.ExecuteNonQuery();
+            connection.Close();
+            this.UpdateGrid();
+            comboBoxNames.Text = textBoxLocation.Text = "";
         }
 
-        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        private void UpdateGrid()
         {
-            if (e.KeyValue == 13)
-                this.button1_Click(null, null);
+            connection.Open();
+
+            if (connection.State != ConnectionState.Open)
+                return;
+
+            command = new OleDbCommand("SELECT * FROM Informacion_universidad.Zona", connection);
+            adapter = new OleDbDataAdapter(command);
+            table = new DataTable();
+            adapter.Fill(table);
+            dataGridViewData.DataSource = table;
+            connection.Close();
         }
     }
 }
